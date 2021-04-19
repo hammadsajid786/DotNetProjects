a34fe1be3db98142b32054c6ed6c2da52ce78690
Hammad Sajid <786hammadsajid@gmail.com>
2021-04-20 02:13:43 +0500

    OcelotApi Gateway
    Create my first solution after learning Ocelot API Gateway with Microservices Architecture.

---
+++ MicroServicesSolution1/MicroServicesSolution1/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroServicesSolution1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+    }
+}
