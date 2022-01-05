54ac6be4c299e8096a2d183fd5dd08fcd9db57cc
Hammad Sajid <786hammadsajid@gmail.com>
2022-01-05 05:22:52 +0500

    Create HomeController.cs

---
+++ DockerDotnetCoreApiTesting/BackendAPIs/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BackendAPIs.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
