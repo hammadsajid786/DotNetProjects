f78e28868d56effff763f270840397d434149742
Hammad Sajid <786hammadsajid@gmail.com>
2022-08-02 18:47:37 +0500

    Binance Bot Invalid API handled with ORders count

--- BinanceBot/BinanceBot/Models/CustomEnums/Messages.cs
+++ BinanceBot/BinanceBot/Models/CustomEnums/Messages.cs
@@ -8,8 +8,10 @@ namespace BinanceBot.Models.CustomEnums
 {
     internal class Messages
     {
+        public static readonly string InsufficientBalance = "Account has insufficient balance for requested action.";
         public static readonly string PurchaseOrderNotCreated = "Purchase order not created.";
         public static readonly string SellOrderNotCreated = "Sell order not created.";
+        public static readonly string InvalidAPIKeyIPOrPermissions = "Invalid API-key, IP, or permissions for action.";
 
     }
 }
